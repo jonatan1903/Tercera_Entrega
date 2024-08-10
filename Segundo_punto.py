1f5d484bc8d8938ae5f1be5a83606fba27ef50d4
Jonathan <jonatannorenagrisales@gmail.com>
2024-08-09 21:41:11 -0500

    Segundo_punto

---
+++ Segundo_punto.py
@@ -0,0 +1,12 @@
+class plano_cartesiano :
+    
+    def __init__(self, x, y, z):
+        self.coordenadaX = x
+        self.coordenadaY = y
+        self.coordenadaZ = z
+    
+    
+XyYyZ = plano_cartesiano( 5, 3, 4)
+
+print(f" el punto inicialmente está en {XyYyZ.coordenadaX, XyYyZ.coordenadaY, XyYyZ.coordenadaZ  }")
+
